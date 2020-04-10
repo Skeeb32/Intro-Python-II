346bbde0007cea41e11506b28ee94bf51bf51014
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-04-10 02:10:50 -0700

    Revert "Intro-Python-II"

--- src/item.py
+++
@@ -1,7 +0,0 @@
-class Item: 
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-
-    def __str__(self):
-        return(f"You have aquired {self.name}/n {self.description}/n")
